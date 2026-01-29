8218b2f1fd6359108dd491640cf080d2d567a192
Vyacheslav142 <109301108+Vyacheslav142@users.noreply.github.com>
2026-01-29 14:43:10 +0700

    2

--- src/database/prisma.ts
+++ src/database/prisma.ts
@@ -1,19 +1,7 @@
 import { PrismaClient } from '@prisma/client';
-import { PgAdapter } from '@prisma/adapter-pg';
-import { Pool } from 'pg';
 import logger from '../config/logger';
 
-const connectionString = process.env.DATABASE_URL;
-
-if (!connectionString) {
-  throw new Error('DATABASE_URL is not set');
-}
-
-const pool = new Pool({ connectionString });
-const adapter = new PgAdapter(pool);
-
 const prisma = new PrismaClient({
-  adapter,
   log:
     process.env.NODE_ENV === 'development' ? ['query', 'info', 'warn', 'error'] : ['warn', 'error'],
 });
